dd92d1d6b4ed6f9c5dabe9ee11d4e57236782156
northernlights15 <northernlights0015@gmail.com>
2015-11-17 00:16:53 +0300

    corrected addition of GameManager and GameCamera scripts

---
+++ CharacterController2D/Assets/GameCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+    public Transform target;
+
+    private float trackSpeed = 10f;
+
+    public void setTarget(Transform t)
+    {
+        target = t;
+    }
+
+	
+	// after player starts moving
+	void LateUpdate () {
+           
+        if (target)
+        {
+            float x = IncrementTowards(transform.position.x, target.position.x, trackSpeed);
+            float y = IncrementTowards(transform.position.y, target.position.y, trackSpeed);
+            transform.position = new Vector3(x, y, transform.position.z);
+        }
+    }
+
+    // Increase n towards target speed
+    private float IncrementTowards(float n, float target, float a)
+    {
+        if (n == target)
+        {
+            return n;
+        }
+        else
+        {
+            // check which direction must current speed be incremented to get to the target speed.
+            float dir = Mathf.Sign(target - n); // n must be increased or decreased to get closer to target.
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n) ? n : target); // if n has passed the target, return the target. Otherwise return n.
+
+        }
+    }
+}
---
+++ CharacterController2D/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+  Will handle a lot of bunch of stuffs like spawning a player, checkpoints etc.
+**/
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+
+    private GameCamera cam;
+
+	void Start () {
+
+        cam = GetComponent<GameCamera>();
+        SpawnPlayer();
+	}
+	
+	private void SpawnPlayer()
+    {
+        cam.setTarget((Instantiate(player, Vector3.zero, Quaternion.identity)as GameObject).transform);
+    }
+
+	void Update () {
+	
+	}
+}
